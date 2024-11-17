d152f75d2fa41c58287c4226e94bb779723f5d1a
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-17 21:04:30 +0100

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,42 +8,25 @@ if (process.argv.length !== 3) {
 }
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const options = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + movieId,
+  method: 'GET'
+};
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  try {
-    const data = JSON.parse(body);
-    const characterUrls = data.characters;
-
-    characterUrls.forEach((url) => {
-      request(url, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error fetching character data:', charError);
-          return;
-        }
-
-        if (charResponse.statusCode !== 200) {
-          console.error(
-            `Failed to fetch character data. Status code: ${charResponse.statusCode}`
-          );
-          return;
-        }
-
-        const character = JSON.parse(charBody);
-        console.log(character.name);
-      });
-    });
-  } catch (parseError) {
-    console.error('Error parsing data:', parseError);
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
